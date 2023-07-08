97c2b306040bb5b8567c24938743dd8af7422d55
#haygoitoilachi <thanhchi.pham.14@gmail.com>
2023-07-08 12:51:12 +0700

    Admin Order Status Update

--- app/Http/Controllers/Backend/OrderController.php
+++ app/Http/Controllers/Backend/OrderController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
 use App\Models\OrderItem;
+use Barryvdh\DomPDF\Facade\Pdf;
+
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -24,7 +26,7 @@ class OrderController extends Controller
 
     public function ConfirmedOrders()
     {
-        $orders = Order::where('status', 'confirmed')->orderBy('id', 'DESC')->get();
+        $orders = Order::where('status', 'confirm')->orderBy('id', 'DESC')->get();
         return view('backend.order.confirmed_orders', compact('orders'));
     }
 
@@ -56,5 +58,81 @@ class OrderController extends Controller
     {
         $orders = Order::where('status', 'cancel')->orderBy('id', 'DESC')->get();
         return view('backend.order.cancel_orders', compact('orders'));
+    }
+
+    public function PendingToConfirm($order_id)
+    {
+        Order::findOrFail($order_id)->update(['status' => 'confirm']);
+
+        $notification = array(
+            'message' => 'Order Confirm Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('pending-orders')->with($notification);
+    }
+
+    public function ConfirmToProcessing($order_id)
+    {
+
+        Order::findOrFail($order_id)->update(['status' => 'processing']);
+
+        $notification = array(
+            'message' => 'Order Processing Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('confirmed-orders')->with($notification);
+    }
+
+
+    public function ProcessingToPicked($order_id)
+    {
+        Order::findOrFail($order_id)->update(['status' => 'picked']);
+
+        $notification = array(
+            'message' => 'Order Picked Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('processing-orders')->with($notification);
+    }
+
+
+    public function PickedToShipped($order_id)
+    {
+        Order::findOrFail($order_id)->update(['status' => 'shipped']);
+
+        $notification = array(
+            'message' => 'Order Shipped Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('picked-orders')->with($notification);
+    }
+
+
+    public function ShippedToDelivered($order_id)
+    {
+        Order::findOrFail($order_id)->update(['status' => 'delivered']);
+
+        $notification = array(
+            'message' => 'Order Delivered Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('shipped-orders')->with($notification);
+    }
+
+    public function AdminInvoiceDownload($order_id)
+    {
+        $order = Order::with('division', 'district', 'province', 'user')->where('id', $order_id)->first();
+        $orderItem = OrderItem::with('product')->where('order_id', $order_id)->orderBy('id', 'DESC')->get();
+
+        $pdf = PDF::loadView('backend.order.order_invoice',compact('order','orderItem'))->setPaper('a4')->setOptions([
+            'tempDir' => public_path(),
+            'chroot' => public_path(),
+        ]);
+    return $pdf->download('invoice.pdf');
     }
 }
--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -69,7 +69,7 @@
     <!-- Sunny Admin App -->
     <script src="backend/js/template.js"></script>
     <script src="backend/js/pages/dashboard.js"></script>
-    
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
     <script>
         @if(Session::has('message'))
@@ -95,33 +95,7 @@
     </script>
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script>
-        $(function() {
-            $(document).on('click', '#delete', function(e) {
-                e.preventDefault();
-                var link = $(this).attr("href");
-                Swal.fire({
-                    title: 'Are you sure?',
-                    text: "Delete this Data!",
-                    icon: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: 'Yes, delete it!'
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        window.location.href = link
-                        Swal.fire(
-                            'Deleted!',
-                            'Your file has been deleted.',
-                            'success'
-                        )
-                    }
-                })
-            })
-        })
-    </script>
-
+    <script src="backend/js/code.js"></script>
 </body>
 
 </html>
